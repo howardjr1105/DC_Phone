8f67a4b12da63c7fc0f56f536670407b1256561c
howardjr1105 <howardescerrato@gmail.com>
2022-06-28 17:27:24 -0600

    Inicio

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+
+
+var map = L.map('mapa').setView([12.138386, -86.227902], 16);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([12.138386, -86.227902]).addTo(map)
+    .bindPopup('DC Phone')
+    .openPopup();
+
+
