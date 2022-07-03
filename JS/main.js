7a9f73fe0bf7e13e0aacde23223cf6f417f2b84c
howardjr1105 <howardescerrato@gmail.com>
2022-07-03 10:35:44 -0600

    agregarproductoCSS

--- JS/main.js
+++ JS/main.js
@@ -1,35 +1,135 @@
 let stockProductos = [
-    { id: 1, nombre: "Redmi Note 11S", cantidad: 1, precio: 210, img: 'https://i.ibb.co/2MyvY8k/redmi-note-11s-5g.png' }
+    { id: 1, nombre: "Redmi Note 11S", cantidad: 1, precio: 210, img: 'https://i.ibb.co/2MyvY8k/redmi-note-11s-5g.png' },
+    { id: 2, nombre: "Galaxy Buds2", cantidad: 1, precio: 125, img: 'https://i.ibb.co/FH6pydS/GNB-thumbnail-galaxy-buds2.webp'},
+    { id: 3, nombre: "Silicone Case", cantidad: 1, precio: 49, img: 'https://i.ibb.co/FXgbhnt/MN653.jpg'},
+    { id: 4, nombre: "Amazfit GTR 3 Pro", cantidad: 1, precio: 230, img: 'https://i.ibb.co/4JNzWvd/3e73a5d17e565bbdba35f1e33fc583b5-2048x2048.webp'}
 ]
 
 let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
 
-const contenedorProductos = document.getElementById('contenedorProd')
+const contenedorTel = document.getElementById('contenedorTel')
+const contenedorCover = document.getElementById('contenedorCover')
+const contenedorAud = document.getElementById('contenedorAud')
+const contenedorSmart = document.getElementById('contenedorSmart')
 const contenedorCarrito = document.getElementById('ContCar')
+const botonVaciar = document.getElementById('Vaciar')
+
+botonVaciar.addEventListener('click', () => {
+    carrito.length = 0
+    actualizarCarrito()
+})
 
 stockProductos.forEach((producto) => {
-    const div = document.createElement('div')
-    div.classList.add('producto')
-    div.innerHTML = `
-    <img src="${producto.img}" alt="">
-    <h3>${producto.nombre}</h3>
-    <p>Precio: $ ${producto.precio}</p>
-    <button id="agregar${producto.id}">Agregar</button>
-    `
-    contenedorProductos.appendChild(div)
-
-    const boton = document.getElementById(`agregar${producto.id}`)
-
-    boton.addEventListener('click', () => {
-        agregarAlCarrito(producto.id)
-    })
+    actualizarCarrito()
+   if (contenedorTel != null) {
+    if (producto.id === 1) {
+        const div = document.createElement('div')
+        div.classList.add('producto')
+        div.innerHTML = `
+        <img src="${producto.img}" alt="">
+        <h3>${producto.nombre}</h3>
+        <p>Precio: $ ${producto.precio}</p>
+        <button id="agregar${producto.id}">Agregar</button>
+        `
+        if(contenedorTel != null){
+        contenedorTel.appendChild(div)
+    
+    
+        const boton = document.getElementById(`agregar${producto.id}`)
+    
+        boton.addEventListener('click', () => {
+            agregarAlCarrito(producto.id)
+        })
+    }
+    }
+   }
+   if (contenedorAud != null) {
+    if (producto.id === 2) {
+        const div = document.createElement('div')
+        div.classList.add('producto')
+        div.innerHTML = `
+        <img src="${producto.img}" alt="">
+        <h3>${producto.nombre}</h3>
+        <p>Precio: $ ${producto.precio}</p>
+        <button id="agregar${producto.id}">Agregar</button>
+        `
+        if(contenedorAud != null){
+        contenedorAud.appendChild(div)
+    
+    
+        const boton = document.getElementById(`agregar${producto.id}`)
+    
+        boton.addEventListener('click', () => {
+            agregarAlCarrito(producto.id)
+        })
+    }
+    }
+   }
+   if (contenedorCover != null) {
+    if (producto.id === 3) {
+        const div = document.createElement('div')
+        div.classList.add('producto')
+        div.innerHTML = `
+        <img src="${producto.img}" alt="">
+        <h3>${producto.nombre}</h3>
+        <p>Precio: $ ${producto.precio}</p>
+        <button id="agregar${producto.id}">Agregar</button>
+        `
+        if(contenedorCover != null){
+        contenedorCover.appendChild(div)
+    
+    
+        const boton = document.getElementById(`agregar${producto.id}`)
+    
+        boton.addEventListener('click', () => {
+            agregarAlCarrito(producto.id)
+        })
+    }
+    }
+   }
+   if (contenedorSmart != null) {
+    if (producto.id === 4) {
+        const div = document.createElement('div')
+        div.classList.add('producto')
+        div.innerHTML = `
+        <img src="${producto.img}" alt="">
+        <h3>${producto.nombre}</h3>
+        <p>Precio: $ ${producto.precio}</p>
+        <button id="agregar${producto.id}">Agregar</button>
+        `
+        if(contenedorSmart != null){
+        contenedorSmart.appendChild(div)
+    
+    
+        const boton = document.getElementById(`agregar${producto.id}`)
+    
+        boton.addEventListener('click', () => {
+            agregarAlCarrito(producto.id)
+        })
+    }
+    }
+   }
 })
 
 const agregarAlCarrito = (prodId) => {
+    let contador = 0
     const item = stockProductos.find((prod) => prod.id === prodId)
-    carrito.push(item)
+    for (let index = 0; index < carrito.length; index++) {
+        if (carrito[index].id === item.id) {
+            carrito[index].cantidad = carrito[index].cantidad + 1;
+            contador = contador + 1
+            stockProductos.forEach((producto) => {
+                if (prodId === producto.id) {
+                    carrito[index].precio = producto.precio * carrito[index].cantidad
+                }
+            });
+        }
+    }
+    if (contador === 0) {
+        carrito.push(item)
+    }
+    
     actualizarCarrito()
-    console.log(carrito)
 }
 
 const eliminarDelCarrito = (prodId) => {
@@ -38,8 +138,7 @@ const eliminarDelCarrito = (prodId) => {
     carrito.splice(indice,1)
     actualizarCarrito()
 }
-
-const actualizarCarrito = () => {
+function actualizarCarrito(){
     contenedorCarrito.innerHTML = ""
 
     carrito.forEach((prod) => {
@@ -54,6 +153,7 @@ const actualizarCarrito = () => {
         contenedorCarrito.appendChild(div)
         
     })
-    localStorage.setItem('carrito', JSON.stringify(a));
+    localStorage.removeItem('carrito')
+    localStorage.setItem('carrito', JSON.stringify(carrito));
 }
 
